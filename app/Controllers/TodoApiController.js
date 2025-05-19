69b536db4516f3d0eca446f769be3231b327c572
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2025-05-19 14:15:50 -0300

    try catch

--- app/Controllers/TodoApiController.js
+++ app/Controllers/TodoApiController.js
@@ -11,6 +11,8 @@ export default (function () {
         // GET /todos
         'list': async (req, res) => {
 
+            // req.query = query vars
+
             const limit = parseInt(req.query.limit) || 100;
             const offset = parseInt(req.query.offset) || 0;
 
@@ -18,39 +20,59 @@ export default (function () {
                 return res.status(400).json({ error: 'Limit máximo: 100.' });
             }
 
+            return res.status(200).json({
+                rows: [],
+                limit: limit
+            });
+
             // TodoModel.findAll(options: {...})
             // limit: int
             // offset: int
             // order: [field, ASC or DESC]
 
-            return res.status(200).json({
-                rows: [],
-                limit: limit
-            });
+            try {
+
+            } catch (error) {
+                return res.status(500).json({ error: 'Error de servidor.' })
+            }
 
         },
 
         // GET /todos/:id
         'get': async (req, res) => {
 
-
-
+            // req.params = parametros da url (id)
 
             const id = req.params.id;
 
             // TodoModel.findByPk(id)
 
             return res.status(404).json({ error: 'Tarefa não encontrada.' });
+
+            try {
+
+            } catch (error) {
+                return res.status(500).json({ error: 'Error de servidor.' })
+            }
         },
 
         // POST /todos
         'insert': async (req, res) => {
+
+            // req.body = request body
+
             const title = req.body.title;
             const is_checked = req.body.is_checked;
 
             // TodoModel.create({ ... });
 
             return res.status(404).json({ error: 'Tarefa não encontrada.' });
+
+            try {
+
+            } catch (error) {
+                return res.status(500).json({ error: 'Error de servidor.' })
+            }
         },
 
         // PUT /todos/:id
@@ -63,14 +85,26 @@ export default (function () {
             // resposta [colunasAtualizadas, [{...}, {...}, ...]]
 
             return res.status(404).json({ error: 'Tarefa não encontrada.' });
+
+            try {
+
+            } catch (error) {
+                return res.status(500).json({ error: 'Error de servidor.' })
+            }
         },
 
         // DELETE /todos/:id
         'delete': async (req, res) => {
             const { id } = req.params;
-
+            // // TodoModel.findByPk(id)
             // todo.destroy
             return res.status(404).json({ error: 'Tarefa não encontrada.' });
+
+            try {
+
+            } catch (error) {
+                return res.status(500).json({ error: 'Error de servidor.' })
+            }
         },
 
     };
